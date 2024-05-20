a702b202e623b76e7a698d755f33d64c9a5f7827
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2024-05-20 20:24:16 +0200

    Add files via upload

---
+++ REFUERZO_AMPLIACION/Pokedex/DAW_ALUMNO/js/script.js
@@ -0,0 +1,66 @@
+let baseDeDatos = [
+    {
+        id: 1,
+        nombre: "Bulbasaur",
+        imagen: "imgs/bulbasaur.png",
+        tipos: ["Grass", "Poison"],
+        info: "Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo"
+    },
+    {
+        id: 7,
+        nombre: "Squirtle",
+        imagen: "imgs/squirtle.png",
+        tipos: ["Water"],
+        info: "Cuando retrae su largo cuello en el caparazón, dispara agua a una presión increíble"
+    },
+    {
+        id: 77,
+        nombre: "Geodude",
+        imagen: "imgs/geodude.png",
+        tipos: ["Rock", "Ground"],
+        info: "Se suele encontrar en senderos de montaña y sitios parecidos. Conviene andar con cuidado para no pisarlo sin querer y provocar su enfado"
+    },
+    {
+        id: 10,
+        nombre: "Caterpie",
+        imagen: "imgs/Caterpie.png",
+        tipos: ["Bug"],
+        info: "Para protegerse, despide un hedor horrible por las antenas con el que repele a sus enemigos"
+    },
+    {
+        id: 2,
+        nombre: "Ivysaur",
+        imagen: "imgs/ivysaur.png",
+        tipos: ["Grass","Poison"],
+        info: "Cuando le crece bastante el bulbo del lomo, pierde la capacidad de erguirse sobre las patas traseras"
+    },
+];
+
+// PENDIENTE COMPLETAR
+const DOMTableBody = document.querySelector("?????????????");
+
+
+// Objeto Pikachu
+const pikachu = {
+    id: 11,
+    nombre: "Pikachu",
+    imagen: "imgs/pikachu.png",
+    tipos: ["Electric"],
+    info: "Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas"
+};
+
+function renderizar() {
+
+
+}
+
+
+
+// ------------------- BOTONERA ------------------
+
+// ---------------------------------------
+// INICIO
+// Se pinta la tabla cuando se carga la página
+//window.onload = renderizar;
+renderizar();
+
